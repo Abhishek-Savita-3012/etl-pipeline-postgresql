9b4d0d61cabec2e2211be2f17440971bec0fea7f
Abhishek-Savita-3012 <asavita3012@gmail.com>
2026-08-03 22:44:19 +0530

    Lesson 12: Build PostgreSQL loading module

---
+++ scripts/load.py
@@ -0,0 +1,90 @@
+from pathlib import Path
+
+import pandas as pd
+import psycopg2
+
+
+def get_connection():
+    """
+    Create PostgreSQL connection.
+    """
+
+    return psycopg2.connect(
+        host="localhost",
+        database="etl_pipeline_db",
+        user="etl_user",
+        password="Abhi30@shek",
+        port=5432
+    )
+
+
+def create_table(conn):
+    """
+    Create customers table.
+    """
+
+    sql_file = Path("sql/create_customers_table.sql")
+
+    with open(sql_file, "r") as file:
+        query = file.read()
+
+    cur = conn.cursor()
+
+    cur.execute(query)
+
+    conn.commit()
+
+    cur.close()
+
+
+def load_data(conn, df):
+    """
+    Load DataFrame into PostgreSQL.
+    """
+
+    cur = conn.cursor()
+
+    inserted = 0
+
+    for _, row in df.iterrows():
+
+        cur.execute(
+            """
+            INSERT INTO customers
+            (customer_id, name, age, city)
+
+            VALUES (%s, %s, %s, %s)
+
+            ON CONFLICT (customer_id)
+            DO NOTHING;
+            """,
+            (
+                int(row["customerid"]),
+                row["name"],
+                int(row["age"]),
+                row["city"]
+            )
+        )
+
+        inserted += 1
+
+    conn.commit()
+
+    cur.close()
+
+    print(f"\nRows Processed : {inserted}")
+
+
+if __name__ == "__main__":
+
+    conn = get_connection()
+
+    create_table(conn)
+
+    df = pd.read_csv("data/processed/customers_clean.csv")
+
+    load_data(conn, df)
+
+    conn.close()
+
+    print("\nData Loaded Successfully!")
---
+++ sql/create_customers_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS customers (
+
+    customer_id INTEGER PRIMARY KEY,
+
+    name VARCHAR(100) NOT NULL,
+
+    age INTEGER,
+
+    city VARCHAR(100)
+
+);
